b298ed8805f3edc11010472f1fe6d90ac27cebf5
Samira2167 <samirahasanppp10@gmail.com>
2026-01-20 03:39:48 +0600

    ProblemN.C

---
+++ Assuit ICPC/Sheet 01/N_Char.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char X;
+    scanf("%c", &X);
+
+    if (X >= 'a' && X <= 'z')
+        X = X - 32;
+    else
+        X = X + 32;
+
+    printf("%c\n", X);
+    return 0;
+}
